bbc5b5dcb67db3b8cc70b19d70bd92fd3ffb3f50
nikitha <nikitha@gmail.com>
2025-01-04 11:19:24 +0530

    nodejs

---
+++ app.java
@@ -0,0 +1,9 @@
+Const express=require('express') ;
+Const app=express() ;
+App get('/', (req, res) =>{
+res. Send("Hello world, Iam from nikitha") ;
+}) ;
+app.listen(3020, () =>{
+Console.log("server is running on port 3020")
+})
+
